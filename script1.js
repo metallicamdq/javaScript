90da3b5f305f97aad6aa37f89f01e4e340904eea
elchoza <schreiber.julian@gmail.com>
2022-03-01 09:30:14 -0300

    version primera entrega

--- script1.js
+++ script1.js
@@ -1,5 +1,6 @@
 //variables simples//
 let preguntas, compra = null;
+let montoTotal
 //constructora//
 function Producto(nombreValor, precioValor, stockValor) {
     this.nombre = nombreValor;
@@ -11,10 +12,10 @@ const productoA = new Producto('casa', 5000, 31);
 const productoB = new Producto('departamento', 3000, 21);
 const productoC = new Producto('campo', 1500, 27);
 const productoD = new Producto('ph', 2500, 17);
-const productoF = new Producto('Local', 3500, 365);
-//array
+const productoF = new Producto('Local', 1500, 365);
+//array//
 const listaProductos = [];
-listaProductos.push(productoA,productoB,productoC,productoD,productoF);
+listaProductos.push(productoA, productoB, productoC, productoD, productoF);
 
 console.log(listaProductos)
 console.log(listaProductos.indexOf(productoF));
@@ -27,6 +28,30 @@ function mostrarDatos(producto, monto) {
     alert('quedan en stock ' + producto + ' ' + 'dias disponibles para alquilar');
     alert('El precio total es de $' + ' ' + monto);
 }
+//funcion calcular el monto//
+function calcularMontoTotal(compra, precio, stock) {
+
+    let montoTotal = compra * precio;
+
+    stock -= compra;
+
+    mostrarDatos(stock, montoTotal)
+
+}
+//funcion mostrar en consola//
+function mostrarEnConsola(resultado){
+    console.log(resultado)
+}
+//find//
+const resultadoDeBusqueda1 = listaProductos.find((el) => el.nombre === 'casa');
+const resultadoDeBusqueda2 = listaProductos.find((el) => el.nombre === 'ph');
+const resultadoDeBusqueda3 = listaProductos.find((el) => el.nombre === 'edificio');
+mostrarEnConsola(resultadoDeBusqueda1);
+mostrarEnConsola(resultadoDeBusqueda2);
+mostrarEnConsola(resultadoDeBusqueda3);
+//filter//
+const resultadoFiltrado = listaProductos.filter((el) => el.precio === 1500);
+mostrarEnConsola(resultadoFiltrado);
 //menu compra//
 function menuDeCompra() {
     do {
@@ -39,29 +64,19 @@ function menuDeCompra() {
         (0) Salir`).toLowerCase();
         if (preguntas == 'casa' || preguntas == 1) {
             compra = prompt('seleccione la cantidad de dias que quiere alquilar');
-            let montoTotalC = compra * listaProductos[0].precio;
-            listaProductos[0].stock -= compra;
-            mostrarDatos(listaProductos[0].stock, montoTotalC);
+            calcularMontoTotal(compra, listaProductos[0].precio, listaProductos[0].stock)
         } else if (preguntas == 'departamento' || preguntas == 2) {
             compra = prompt('seleccione la cantidad de dias que quiere alquilar el producto');
-            let montoTotalD = compra * listaProductos[1].precio;
-            listaProductos[1].stock -= compra;
-            mostrarDatos(listaProductos[1].stock, montoTotalD);
+            calcularMontoTotal(compra, listaProductos[1].precio, listaProductos[1].stock)
         } else if (preguntas == 'campo' || preguntas == 3) {
             compra = prompt('seleccione la cantidad de dias que quiere alquilar el producto');
-            let montoTotalF = compra * listaProductos[2].precio;
-            listaProductos[2].stock -= compra;
-            mostrarDatos(listaProductos[2].stock, montoTotalF);
+            calcularMontoTotal(compra, listaProductos[2].precio, listaProductos[2].stock)
         } else if (preguntas == 'ph' || preguntas == 4) {
             compra = prompt('seleccione la cantidad de dias que quiere alquilar el producto');
-            let montoTotalG = compra * listaProductos[3].precio;
-            listaProductos[3].stock -= compra;
-            mostrarDatos(listaProductos[3].stock, montoTotalG);
+            calcularMontoTotal(compra, listaProductos[3].precio, listaProductos[3].stock)
         } else if (preguntas == 'local' || preguntas == 5) {
             compra = prompt('seleccione la cantidad de dias que quiere alquilar el producto');
-            let montoTotalH = compra * listaProductos[3].precio;
-            listaProductos[4].stock -= compra;
-            mostrarDatos(listaProductos[4].stock, montoTotalH);
+            calcularMontoTotal(compra, listaProductos[4].precio, listaProductos[4].stock)
         } else if (preguntas == 0 || preguntas == 'salir') {
             break;
         } else {
